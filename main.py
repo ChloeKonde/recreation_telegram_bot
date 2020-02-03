a05c9507e6f0d53711b716ccdfe4f0a7b5544f95
Хлоя Конде <konde.chloe@gmail.com>
2020-02-03 19:11:04 +0700

    Added text for additional information

--- main.py
+++ main.py
@@ -5,32 +5,24 @@ from telegram import KeyboardButton, ReplyKeyboardMarkup
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                      level=logging.INFO)
 
-REQUEST_KWARGS={
-    'proxy_url': 'socks5://138.201.243.83:1080',
-}
-
-updater = Updater(token='token', use_context=True,  request_kwargs=REQUEST_KWARGS)
+updater = Updater(token='912658745:AAEvvAGV-L5ajB_wkAW74eZq52K7rPtZSSM', use_context=True)
 dispatcher = updater.dispatcher
 
 def start(update, context):
 	context.bot.send_message(chat_id=update.message.chat_id,
-							text='Hello. Choose your floor', 
+							text='Hello, ' + update.message.chat.username + ' Use keyboard for getting information.', 
 							reply_markup=start_menu())
 
 def start_menu():
-	third_floor = KeyboardButton(text='3')
-	forth_floor = KeyboardButton(text='4')
+	button = KeyboardButton(text='Get information')
+	return ReplyKeyboardMarkup([[button]], resize_keyboard=True)	
 
-	return ReplyKeyboardMarkup(resize_keyboard=True, [ [f, h], [third_floor, forth_floor]])
-
-def send_third_floor(update, context):
-	context.bot.send_photo(chat_id=update.message.chat_id, photo=open('1.jpg', 'rb'), caption='Third floor')
-
-def send_forth_floor(update, context):
-	context.bot.send_photo(chat_id=update.message.chat_id, photo=open('2.jpg', 'rb'), caption="Forth floor")
-
+def send_information(update, context):
+	context.bot.send_photo(chat_id=update.message.chat_id, photo=open('1.jpg', 'rb'))
+	with open('t.txt') as f:
+		for line in f.readlines():
+			context.bot.send_message(chat_id=update.message.chat_id, text=line)
 
 dispatcher.add_handler(CommandHandler('start', start))
-dispatcher.add_handler(MessageHandler(Filters.regex('3'), send_third_floor))
-dispatcher.add_handler(MessageHandler(Filters.regex('4'), send_forth_floor))
+dispatcher.add_handler(MessageHandler(Filters.regex('Get information'), send_information))
 updater.start_polling()
